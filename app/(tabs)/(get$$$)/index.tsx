188eecbba4b30a719db768aa894191c59729a070
Brosaidon <pandawab@me.com>
2025-10-02 11:05:23 +0200

    🌅 background

--- app/(tabs)/(get$$$)/index.tsx
+++ app/(tabs)/(get$$$)/index.tsx
@@ -3,7 +3,14 @@ import { ProgressBar } from "@/components/progress-bar";
 import { useBank } from "@/context/bankProvider";
 import { mockedBuildings } from "@/scripts/buildings";
 import { useState } from "react";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function TabsHome() {
   const [goldKey, setGoldKey] = useState(0);
@@ -16,52 +23,58 @@ export default function TabsHome() {
   }
 
   return (
-    <View style={{ flex: 1 }}>
+    <ImageBackground
+      source={require("@/assets/images/bg.png")}
+      style={styles.Background}
+      resizeMode="cover"
+    >
       <View style={{ flex: 1 }}>
-        <Gyros
-          onGyroTrigger={() => {
-            setTreeProgress((prev) => {
-              const newProgress = prev + 0.2; // 1 av 5
-              return newProgress > 1 ? 1 : newProgress;
-            });
-          }}
-        />
-        <View>
-          <Text>{bank.gold}</Text>
-          <Text>{bank.tree}</Text>
-          {mockedBuildings.map((b) => (
-            <Text key={b.bankRef}>
-              {b.name} : {bank[b.bankRef] ? "Owned" : "Not Owned"}
-            </Text>
-          ))}
+        <View style={{ flex: 1 }}>
+          <Gyros
+            onGyroTrigger={() => {
+              setTreeProgress((prev) => {
+                const newProgress = prev + 0.2; // 1 av 5
+                return newProgress > 1 ? 1 : newProgress;
+              });
+            }}
+          />
+          <View>
+            <Text>{bank.gold}</Text>
+            <Text>{bank.tree}</Text>
+            {mockedBuildings.map((b) => (
+              <Text key={b.bankRef}>
+                {b.name} : {bank[b.bankRef] ? "Owned" : "Not Owned"}
+              </Text>
+            ))}
+          </View>
+        </View>
+        <Pressable onPress={startGoldProgress} style={styles.clickerRow}>
+          <Image
+            source={require("../../../assets/images/gold.webp")}
+            style={{ width: 100, height: 100, tintColor: "#FFD700" }}
+          />
+          <ProgressBar
+            key={goldKey}
+            duration={4000}
+            autoFill
+            onComplete={increaseGold}
+          />
+        </Pressable>
+        <View style={styles.clickerRow}>
+          <Image
+            source={require("@/assets/images/log.webp")}
+            style={{ width: 100, height: 100, tintColor: "#8d3a0344" }}
+          />
+          <ProgressBar
+            progress={treeProgress}
+            onComplete={() => {
+              increaseTree();
+              setTreeProgress(0); // reset bar
+            }}
+          />
         </View>
       </View>
-      <Pressable onPress={startGoldProgress} style={styles.clickerRow}>
-        <Image
-          source={require("../../../assets/images/gold.webp")}
-          style={{ width: 100, height: 100, tintColor: "#FFD700" }}
-        />
-        <ProgressBar
-          key={goldKey}
-          duration={4000}
-          autoFill
-          onComplete={increaseGold}
-        />
-      </Pressable>
-      <View style={styles.clickerRow}>
-        <Image
-          source={require("@/assets/images/log.webp")}
-          style={{ width: 100, height: 100, tintColor: "#8d3a0344" }}
-        />
-        <ProgressBar
-          progress={treeProgress}
-          onComplete={() => {
-            increaseTree();
-            setTreeProgress(0); // reset bar
-          }}
-        />
-      </View>
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -69,6 +82,11 @@ const styles = StyleSheet.create({
   clickerRow: {
     backgroundColor: "#003333",
     flexDirection: "row",
+    alignItems: "center",
+  },
+  Background: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
   },
 });
