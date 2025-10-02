a471d20a0f44e41922a2a442db5cb9c3033cfcfb
Brosaidon <pandawab@me.com>
2025-10-02 19:16:48 +0200

    🌅

--- app/(tabs)/build/[id].tsx
+++ app/(tabs)/build/[id].tsx
@@ -1,6 +1,7 @@
 import { useBank } from "@/context/bankProvider";
 import { mockedBuildings } from "@/scripts/buildings";
 import { Button } from "@react-navigation/elements";
+import { ImageBackground } from "expo-image";
 import { useLocalSearchParams } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -10,8 +11,12 @@ export default function BuildDetailsScreen() {
   const building = mockedBuildings.find((b) => b.id === id);
   if (!building) return <Text>Building not found</Text>;
   return (
-    <>
-      <View>
+    <ImageBackground
+      source={require("@/assets/images/construction.jpg")}
+      style={styles.bg}
+      contentFit="fill"
+    >
+      <View style={{ marginTop: 200 }}>
         <Text>{building.name}</Text>
       </View>
       <View style={{ height: 50 }}>
@@ -29,11 +34,15 @@ export default function BuildDetailsScreen() {
           </Button>
         )}
       </View>
-    </>
+    </ImageBackground>
   );
 }
 const styles = StyleSheet.create({
   buyBuildingButton: {
+    backgroundColor: "#FFF",
     alignItems: "center",
+  },
+  bg: {
+    height: "100%",
   },
 });
